cb1dc0588662ef2b6b59ca6a8efacaac0320c929
Cucumbersaurus <cucumbersaurus@gmail.com>
2022-03-04 11:11:26 +0900

    --all

---
+++ src/main/java/rpgProject/rpg/Rpg.java
@@ -0,0 +1,17 @@
+package rpgProject.rpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rpg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
