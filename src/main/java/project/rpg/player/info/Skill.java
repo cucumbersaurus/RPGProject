2f1ab785db56c98fbbcf0176bb8aaca88f81c7e9
cucumbersaurus <tallcucumber1234@gmail.com>
2022-07-08 17:12:55 +0900

    임시저장

--- src/main/java/project/rpg/player/info/Skill.java
+++ src/main/java/project/rpg/player/info/Skill.java
@@ -14,6 +14,25 @@ public class Skill {
     private final Map<String, SkillBase> _skillMap = new HashMap<>();  //스킬이 하나 말고 여러개이기 때문   위에 uuid는 나중에
     private final Player _player;
 
+    public static void addSkill(Player player, SkillBase skill){  //스킬 추가하기 static  addPlayer 동합
+        Map<String, SkillBase> skills = _skills.get(player).getSkillMap();
+        if (_skills.containsKey(player)) {
+            skills.put(skill._name, skill);
+        } else {
+            if (skill!=null) {
+                skills = new HashMap<>();
+                skills.put(skill._name, skill);
+            }
+        }
+    }
+
+    public void addSkill(SkillBase skillBase) {  //스킬 추가하기 not static
+        addSkill(_player,skillBase);
+    }
+
+    public static void removePlayer(Player player){  //플레이어 삭제
+        _skills.remove(player);
+    }
     public static List<SkillBase> getSkillList(Player player){ //전체 스킬 목록 가져오기
         Skill skills = _skills.get(player);
 
@@ -34,26 +53,6 @@ public class Skill {
 
     public SkillBase getSkill(String skillName) {
         return getSkill(_player,skillName);
-    }
-
-    public static void addSkill(Player player, SkillBase skill){  //스킬 추가하기 static  addPlayer 동합
-        Map<String, SkillBase> skills = _skills.get(player).getSkillMap();
-        if (_skills.containsKey(player)) {
-            skills.put(skill._name, skill);
-        } else {
-            if (skill!=null) {
-                skills = new HashMap<>();
-                skills.put(skill._name, skill);
-            }
-        }
-    }
-
-    public void addSkill(SkillBase skillBase) {  //스킬 추가하기 not static
-        addSkill(_player,skillBase);
-    }
-
-    public static void removePlayer(Player player){  //플레이어 삭제
-        _skills.remove(player);
     }
 
     public Skill(Player player, SkillBase skill) {
