aed4623550bbe07541fd7fbeb5296ffe9e4ffe84
cucumbersaurus <tallcucumber1234@gmail.com>
2022-07-31 12:43:13 +0900

    tp arrow 아이템 추가

--- src/main/java/project/rpg/items/Items.kt
+++ src/main/java/project/rpg/items/Items.kt
@@ -7,14 +7,16 @@ import project.rpg.items.weapon.*
 
 enum class Items(val value: Int, val itemBase: ItemBase?, val type:ItemType, val _name : String) {
 
-    NULL(0, null,ItemType.NULL,"null"),
-    WAND(1, Wand,ItemType.WEAPON,"wand"),
-    MANA_REFILLING_POTION(2, ManaRefillPotion,ItemType.POTION,"mana_refilling_potion"),
-    THE_METEOR(3, TheMeteor,ItemType.WEAPON,"meteor"),
-    TP_SWORD(4, TpSword,ItemType.WEAPON,"tp_sword"),
-    SPEED_APPLE(5, SpeedApple,ItemType.FOOD,"speed_apple"),
-    BLAZING_MARK(6, BlazingMark,ItemType.WEAPON,"blazing_mark"),
-    FLAME_BURST(7, FlameBurst,ItemType.WEAPON,"flame_burst");
+    NULL(0, null, ItemType.NULL,"null"),
+    WAND(1, Wand, ItemType.WEAPON,"wand"),
+    MANA_REFILLING_POTION(2, ManaRefillPotion, ItemType.POTION,"mana_refilling_potion"),
+    THE_METEOR(3, TheMeteor, ItemType.WEAPON,"meteor"),
+    TP_SWORD(4, TpSword, ItemType.WEAPON,"tp_sword"),
+    SPEED_APPLE(5, SpeedApple, ItemType.FOOD,"speed_apple"),
+    BLAZING_MARK(6, BlazingMark, ItemType.WEAPON,"blazing_mark"),
+    FLAME_BURST(7, FlameBurst, ItemType.WEAPON,"flame_burst"),
+    TP_ARROW(8, TpArrow, ItemType.WEAPON, "tp_arrow"),
+    ;
 
     val item: ItemStack?
         get() = itemBase!!.item
---
+++ src/main/java/project/rpg/items/weapon/TpArrow.kt
@@ -0,0 +1,36 @@
+package project.rpg.items.weapon
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextColor
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.block.Action
+import org.bukkit.inventory.ItemStack
+import project.rpg.items.Items
+import project.rpg.skill.tmp.TpArrow
+import project.rpg.textComponets.color.TextColors
+
+object TpArrow: WeaponBase() {
+    override fun createItem() {
+        val item = ItemStack(Material.ARROW)
+        val meta = item.itemMeta
+        skill = TpArrow()
+
+        meta.displayName(Component.text("시공간의 화살").color(TextColor.color(TextColors.DEEP_SKY_BLUE.color)))
+        meta.lore(itemLore())
+        meta.setCustomModelData(Items.TP_ARROW.value)
+
+        item.itemMeta = meta
+        super.item = item
+    }
+
+    private fun itemLore(): List<Component> {
+        val lore = ArrayList<Component>()
+        lore.add(Component.text("핑! 피용!"))
+        return lore
+    }
+
+    override fun onEnable(action: Action?, player: Player) {
+        skill.onEnable(player, action)
+    }
+}
