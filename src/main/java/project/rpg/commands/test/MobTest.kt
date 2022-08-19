4c465bb5dedf1c2855fbeb265a8d3b595479a1f2
washappy <fireballdongheon@gmail.com>
2022-08-19 23:48:03 +0900

    ?

--- src/main/java/project/rpg/commands/test/MobTest.kt
+++ src/main/java/project/rpg/commands/test/MobTest.kt
@@ -1,5 +1,6 @@
 package project.rpg.commands.test
 
+import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -10,9 +11,35 @@ import project.rpg.mob.ZombieKing
 
 class MobTest:CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        if(sender is Player){
-            sender.world.spawnEntity(sender.location, EntityType.ZOMBIE,CreatureSpawnEvent.SpawnReason.CUSTOM, ZombieKing().createMobFun())
+        if (sender is Player) {
+            if (args != null) {
+                return when (args.size) {
+                    0 -> {
+                        sender.world.spawnEntity(sender.location,
+                            EntityType.ZOMBIE,
+                            CreatureSpawnEvent.SpawnReason.CUSTOM,
+                            ZombieKing().createMobFun())
+                        true
+                    }
+                    3 -> {
+                        sender.world.spawnEntity(Location(sender.world,args[0].toDouble(),args[1].toDouble(),args[2].toDouble()),
+                            EntityType.ZOMBIE,
+                            CreatureSpawnEvent.SpawnReason.CUSTOM,
+                            ZombieKing().createMobFun())
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            } else {
+                sender.world.spawnEntity(sender.location,
+                    EntityType.ZOMBIE,
+                    CreatureSpawnEvent.SpawnReason.CUSTOM,
+                    ZombieKing().createMobFun())
+                return true
+            }
         }
-            return true
+        return false
     }
 }
---
+++ src/main/java/project/rpg/mob/MobManager.java
@@ -0,0 +1,4 @@
+package project.rpg.mob;
+
+public class MobManager {
+}
