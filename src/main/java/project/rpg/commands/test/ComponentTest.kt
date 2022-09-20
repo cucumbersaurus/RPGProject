2fbaff3c8ceb398c31cbf7b4b2ec4ac9f79bf239
cucumbersaurus <tallcucumber1234@gmail.com>
2022-09-20 21:44:30 +0900

    textComponent 관련 dsl 연구

---
+++ src/main/java/project/rpg/commands/test/ComponentTest.kt
@@ -0,0 +1,14 @@
+package project.rpg.commands.test
+
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.TextComponent
+
+class ComponentTest {
+
+    fun getComponent(): TextComponent {
+        val component = text("")
+
+        return component
+    }
+
+}
