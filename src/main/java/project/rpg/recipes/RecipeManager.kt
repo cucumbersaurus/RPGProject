6fa46acbb82de6e3cae7d3465ff06f74f67f1ed5
cucumbersaurus <tallcucumber1234@gmail.com>
2023-12-27 22:13:46 +0900

    커스텀 레시피 구현중
    버그 많음

--- src/main/java/project/rpg/recipes/RecipeManager.kt
+++ src/main/java/project/rpg/recipes/RecipeManager.kt
@@ -1,10 +1,11 @@
 package project.rpg.recipes
 
 import org.bukkit.inventory.ItemStack
+import kotlin.math.min
 
 object RecipeManager {
 
-    private val recipeDictionary = HashMap<Recipes, CraftRecipe?>()
+    private val recipeDictionary = HashMap<Recipes, CraftRecipe>()
 
     fun initialize() {
         for (i in Recipes.values()) {
@@ -15,11 +16,28 @@ object RecipeManager {
     fun getRecipe(matrix: List<ItemStack?>): CraftRecipe?{
         var recipe: CraftRecipe? = null
         recipeDictionary.forEach { (_, craftRecipe) ->
-            recipe = craftRecipe!!.matches(matrix)
+            recipe = craftRecipe.matches(matrix)
             if(recipe!=null){
                 return@forEach
             }
         }
         return recipe
+    }
+
+    fun getMaxRecipeCount(matrix: List<ItemStack?>):Int{
+        var min = 0//초기화 해야해서 0으로 둠
+
+        for(i in matrix){
+            if(i!=null){
+                min = i.amount//아이템이 하나라도 있으면 1로 초기화됨
+                break
+            }
+        }
+        for (i in matrix){
+            if(i!=null){
+                min = min(min, i.amount)
+            }
+        }
+        return min
     }
 }
--- src/main/java/project/rpg/recipes/ShapedRecipe.kt
+++ src/main/java/project/rpg/recipes/ShapedRecipe.kt
@@ -6,15 +6,14 @@ interface ShapedRecipe : CraftRecipe {
 
     override val recipe: List<ItemStack?>
     override fun matches(matrix: List<ItemStack?>): CraftRecipe? {
-        require(recipe.size == matrix.size) { "recipe size mismatch" }
+        require(recipe.size == matrix.size)
         for (i in matrix.indices) {
 
             val item = matrix[i]?.clone()
             item?.amount=1
-            //if(recipe[i]!=null && mat[i]!=null && recipe[i]!!.type == mat[i]!!.type && recipe[i]!!.amount > mat[i]!!.amount) return null
             if (recipe[i] != item) return null
         }
+        //TODO("개수 여러개일때 무한 복사 오류 있음")
         return this
-        TODO("개수 여러개일때 무한 복사 오류 있음")
     }
 }
